33cb31cfd6f6effc5d5f4e512f7f91a5a4119da3
rodskades <rodolpho_kades@hotmail.com>
2022-11-29 14:29:02 -0300

    Set the urls in django project

--- rodolpho_kades/urls.py
+++ rodolpho_kades/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from .secrets import theAdmin  # Secret admin route
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path(theAdmin, admin.site.urls),
+    path('', include('core.urls')),                   # Index is on core app.
+    path('biblioteca/', include('biblioteca.urls')),  # My personal library app.
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
